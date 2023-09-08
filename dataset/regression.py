a6e3e7598280bcbba3643815e3648c628276213b
Vlad <saifulinvladislav@gmail.com>
2023-09-09 03:46:29 +0500

    Added Regression for ranking video corpus

---
+++ dataset/regression.py
@@ -0,0 +1,23 @@
+from sklearn.linear_model import LogisticRegression
+
+
+def get_scores(X, y, x_predict, return_names=False, return_only_names=False):
+    if return_only_names is True and return_names is False:
+        raise ValueError("return_only_names cannot be True if return_names is False")
+    clf = LogisticRegression().fit(X, y)
+    scores = clf.predict_proba(x_predict)
+    if return_names is False:
+        return scores
+    else:
+        classes = clf.classes_
+        scores = scores.tolist()[0]
+        classes = [(classes[i], scores[i]) for i in range(len(scores))]
+        classes.sort(key=lambda x: x[1], reverse=True)
+        if return_only_names is True:
+            classes = [i[0] for i in classes]
+        return classes
+
+
+X, y = [[0, 0, 2, 6], [1, 1, 4, 2]], ['video_1', 'video_2']
+x_predict = [[1, 2, 3, 1]]
+print(get_scores(X, y, x_predict, return_names=True, return_only_names=True))
